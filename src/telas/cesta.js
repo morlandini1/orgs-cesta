85bd271cca0baa71af8b1f97f004e32167b097c8
MARCIO ORLANDINI <morlandini1@gmail.com>
2022-05-03 11:19:13 -0300

    Detalhes da cesta

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 
 import topo from '../../assets/topo.png'
 
@@ -9,6 +9,17 @@ export default function Cesta() {
     return <>
     <Image source={topo} style={styles.topo} />
     <Text style={styles.titulo}>Detalhe da cesta</Text>
+
+    <View>
+        <Text style={styles.nome}>Cesta de Verduras</Text>
+        <Text>Janny Jack Farm</Text>
+        <Text>Uma cesta com produtos selecionados 
+            cuidadosamente da fazenda para 
+            sua cozinha</Text>
+        <Text>R$ 40,00</Text>
+            
+
+    </View>
     </>
 }
 
@@ -26,5 +37,11 @@ titulo: {
     color: "white",
     fontWeight: "bold",
     padding: 16,
-}
+},
+nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: "bold",
+  }
 });
