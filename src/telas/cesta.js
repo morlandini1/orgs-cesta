0370866866bb3f445edf252a8125c9c7552ab25b
MARCIO ORLANDINI <morlandini1@gmail.com>
2022-05-03 10:51:52 -0300

    Detalhes da cesta

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -8,7 +8,7 @@ const width = Dimensions.get('screen').width;
 export default function Cesta() {
     return <>
     <Image source={topo} style={styles.topo} />
-    <Text></Text>
+    <Text style={styles.titulo}>Detalhe da cesta</Text>
     </>
 }
 
@@ -16,5 +16,15 @@ const styles = StyleSheet.create({
 topo: {
     width: "100%",
     height: 578 / 768 * width,
+},
+titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 16,
+    color: "white",
+    fontWeight: "bold",
+    padding: 16,
 }
 });
