c62edc8fdbf4b991cc9e9bba4ab1752eb9231716
MARCIO ORLANDINI <morlandini1@gmail.com>
2022-05-04 16:16:14 -0300

    Styles

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 
 import topo from '../../assets/topo.png'
+import logo from '../../assets/logo.png'
 
 const width = Dimensions.get('screen').width;
 
@@ -10,13 +11,15 @@ export default function Cesta() {
     <Image source={topo} style={styles.topo} />
     <Text style={styles.titulo}>Detalhe da cesta</Text>
 
-    <View>
+    <View style={styles.cesta}>
         <Text style={styles.nome}>Cesta de Verduras</Text>
-        <Text>Janny Jack Farm</Text>
-        <Text>Uma cesta com produtos selecionados 
+        <Text style={styles.nomeFazenda}>Janny Jack Farm</Text>
+        <Image source={logo} />
+        <Text style={styles.descricao}>
+            Uma cesta com produtos selecionados 
             cuidadosamente da fazenda para 
             sua cozinha</Text>
-        <Text>R$ 40,00</Text>
+        <Text style={styles.preco}>R$ 40,00</Text>
             
 
     </View>
@@ -38,10 +41,30 @@ titulo: {
     fontWeight: "bold",
     padding: 16,
 },
+cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+},
 nome: {
     color: "#464646",
     fontSize: 26,
     lineHeight: 42,
     fontWeight: "bold",
-  }
+  },
+nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+},
+descricao: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26,
+},
+preco: {
+    color: "#2A9F85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
+}  
 });
