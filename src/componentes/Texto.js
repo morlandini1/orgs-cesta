ed6dcdbdd5052718fc81fcac0ecb54c6bbfcd72a
MARCIO ORLANDINI <morlandini1@gmail.com>
2022-05-05 17:22:44 -0300

    update

--- src/componentes/Texto.js
+++ src/componentes/Texto.js
@@ -2,7 +2,13 @@ import react from "react";
 import { StyleSheet, Text } from "react-native";
 
 export default function Texto({ children, style }) {
-    return <Text style={[style, styles.texto]}>{ children }</Text>
+   let style = styles.texto;
+
+if (style?.fontweight === 'bold') {
+    style = styles.textoNegrito;
+}
+  return <Text style={[style, styles.texto]}>{ children }</Text>
+
 }
 
 const styles = StyleSheet.create({
