9e4a323c50a2a25111227ee20c4d7a14dbc773cf
MARCIO ORLANDINI <morlandini1@gmail.com>
2022-05-05 16:01:02 -0300

    Install Google fonts

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import react from 'react';
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold 
+} from '@expo-google-fonts/montserrat';
 
-import Cesta from './src/telas/cesta'
+import Cesta from './src/telas/cesta';
 
 export default function App() {
+  const [fontecarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fontecarregada){
+    return <View />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
